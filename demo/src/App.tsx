0c21406673c8d96a6e413931662253ce09f472ad
Adir <slutzkis@gmail.com>
2025-07-29 15:13:18 +0300

    design

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css'
 import './globals.css'
+
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Wrench } from 'lucide-react';
 
 function App() {
 
   return (
     <>
-      <Card className='w-50 items-start'>
+      <Card className='w-50 items-start border-none hover:bg-popover'>
         <CardHeader>
-          <CardTitle className='text-left'>Title</CardTitle>
+          <CardTitle className='flex gap-1 text-left font-normal text-sm'>
+            <Wrench size={16} />
+            Title
+          </CardTitle>
           <CardDescription>Subtitle</CardDescription>
         </CardHeader>
       </Card>
